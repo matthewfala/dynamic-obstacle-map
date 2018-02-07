2f4270df9fc76f3e7c420fe43c800193960e06b1
Callum <callumpe@usc.edu>
2018-02-06 22:56:15 -0800

    Defines an empty Gameobject which instantiates and controls cube objects

---
+++ unity/examples/Simulator/Assets/State.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public class State
+	{
+		public int x,y,z;
+		public State (int a, int b, int c)
+		{
+			x = a;
+			y = b;
+			z = c;
+		}
+		int [] getCoor()
+		{
+			return new int[] {x,y,z};
+		}
+
+	}
+}
+
