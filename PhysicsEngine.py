b905c348cd0eec0d01dbfa52beab42bc059bb2f5
Callum <callumpe@usc.edu>
2018-02-20 22:27:30 -0800

    Added rotation update function

--- PhysicsEngine.py
+++ PhysicsEngine.py
@@ -1,3 +1,5 @@
+import numpy as np
+import math
 class World:
     'World class for the physics engine. It is essential a robot class since the robot is the center of the world, which means that everything will move relatively to the robot.'
 
@@ -6,32 +8,40 @@ class World:
 
     # constructor
     def __init__(self):
-        self.actorList = {} # list of actors
+        self.actorList = [] # list of actors
         self.velocity = [0, 0, 0] # velocity of the world
-        self.facing = [1, 0, 0] # initially facing x axis
-
+        #self.facing = [1, 0, 0] # initially facing x axis
+        self.a = 0
+        self.w = math.pi/4
+        self.t = 1
     # main functions
     def update(self): # keep updating the postiion of all objects
-
-    def create_object(self, name, position, velocity):
-        self.actorList[self.numOfObjects] = Actor(name, position, velocity)
-
+        for a in self.actorList:
+            a.update(self.w,self.a,self.t)
+    
+        
+    def create_object(self, name, position):
+        self.actorList.append(Actor(name, position))
     def move(self, direction, acceleration):
         # update velocity and position
-
+        return 0
 
     # helper functions
     def display_objects(self):
         print self.actorList
 
 
-class Actor:
+
+class Actor():
     'Base class for all objects in the world.'
-    
+   
     def __init__(self, name, position):
         self.name = name
         self.position = position
-
+    def update(self,w,a,t):
+        theta = w * t + .5 * math.pow(a,t) 
+        m = np.array([[math.cos(theta), -math.sin(theta)],[math.sin(theta), math.cos(theta)]])
+        self.position = m.dot(self.position)
 class Gate:
     'Gate class for challenge 1.'
 
@@ -66,9 +76,7 @@ class Roulette:
     def __init__(self, name, position):
         self.name = name
         self.position = position
-
-
-
-# initialization
-x = World()
-x.create_object
+r = World()
+r.create_object("one",np.array([0,1]))
+r.update()
+r.actorList[0].position
