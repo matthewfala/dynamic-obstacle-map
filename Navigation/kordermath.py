c293166dc960900f5271bc2e154f0f67c99c3f79
Kishore Venkateshan <kishore.venkateshan1729@gmail.com>
2018-03-11 01:05:32 -0500

    KOrder Math now has a quaternion class to rotate vectors

--- Navigation/kordermath.py
+++ Navigation/kordermath.py
@@ -1,23 +1,31 @@
 import numpy as np
 from numpy import linalg as nla
+from math import sin, cos, acos, sqrt
 
 ###########################################################################################
 # KORDERMATH is the high level namespace for our Math Commons.
 ###########################################################################################
 class KOrderMath:
-    def convert_degrees_to_radians(self, degrees):
+    @classmethod
+    def convert_degrees_to_radians(cls, degrees):
         return np.deg2rad(degrees)
 
-    def convert_radians_to_degrees(self, degrees):
+    @classmethod
+    def convert_radians_to_degrees(cls, degrees):
         return np.rad2deg(degrees)
 
-    def get_projection(self, of_vector, on_vector):
+    @classmethod
+    def get_projection(cls, of_vector, on_vector):
+        if not isinstance(of_vector, KOrderMath.Vector) or not isinstance(on_vector, KOrderMath.Vector):
+            raise ValueError("You can only get projection of one KOrderMath.Vector on another KOrderMath.Vector")
+
         on_vector_mag = on_vector.magnitude()
         on_vector_norm = on_vector / on_vector_mag
         projection_comp = (of_vector % on_vector) / (on_vector_mag)
         projection = projection_comp * on_vector_norm
         return projection
 
+    """
     # Euler vector rotation
     # Ref pg 13 https://www.cs.utexas.edu/~theshark/courses/cs354/lectures/cs354-14.pdf
     def euler_vector_rotate(v, alpha, beta, gamma):
@@ -41,6 +49,7 @@ class KOrderMath:
 
         # return fully rotated array
         return v_gamma
+    """
 
     ###########################################################################################
     # VECTOR respresents a point or a direction in 3D space.
@@ -61,14 +70,22 @@ class KOrderMath:
             return cls(npvector[0], npvector[1], npvector[2])
 
         def __add__(self, other):
+            if not isinstance(other, KOrderMath.Vector):
+                raise ValueError("You can only add a KOrderMath.Vector to another KOrderMath.Vector")
+
             return self.from_npvector(self.npvector + other.npvector)
 
         def __sub__(self, other):
+            if not isinstance(other, KOrderMath.Vector):
+                raise ValueError("You can only subtract a KOrderMath.Vector to another KOrderMath.Vector")
+
             return self.from_npvector(self.npvector - other.npvector)
         
         def __mul__(self, other):
-            if type(other) is type(self):
+            if isinstance(other, KOrderMath.Vector):
                 return self.from_npvector(self.npvector * other.npvector)
+            elif isinstance(other, KOrderMath.Quaternion):
+                return other * self
             else:
                 return self.from_npvector(self.npvector * other)
         
@@ -89,12 +106,18 @@ class KOrderMath:
         # Use mod operator to calculate the dot product between two vectors.
         # Eg. v1 % v2, gives the dot product of two vectors
         def __mod__(self, other):
+            if not isinstance(other, KOrderMath.Vector):
+                raise ValueError("You can only use dot between two KOrderMath.Vector s")
+
             return np.dot(self.npvector, other.npvector)
 
         # Use floor div operator to calculate the cross product between two vectors.
         # Eg. v1 // v2, gives the cross product of the two vectors.
         # Note v1 // v2 is not equal to v2 // v1
         def __floordiv__(self, other):
+            if not isinstance(other, KOrderMath.Vector):
+                raise ValueError("You can only use cross product between two KOrderMath.Vector s")
+
             return self.from_npvector(np.cross(self.npvector, other.npvector))
 
         # To string overload
@@ -112,7 +135,118 @@ class KOrderMath:
                     raise ValueError("The Vector you are trying to normalize has 0 length")
                 return self
             return self.from_npvector(self.npvector / length)
+
+
+    ###########################################################################################
+    # QUTERNION respresents a point or a direction in 3D space.
+    # Our co-ordinate system will follow the convention:
+    # X Axis is forward
+    # Y Axis is right
+    # Z Axis is up
+    ###########################################################################################
+    
+    class Quaternion:
+        def normalize(self, raise_exception=False):
+            length = nla.norm(self.npvector, axis=0)
+            if np.isclose(length, 0.0):
+                if raise_exception:
+                    raise ValueError("The Vector you are trying to normalize has 0 length")
+                return self
+            return self.from_npvector(self.npvector / length)
+
+        def get_axisangle(self):
+            axis = KOrderMath.Vector.from_npvector(self.npvector[1:])
+            axis = axis.normalize()
+
+            angle = acos(self.w) * 2.0
+
+            return angle, axis
+
+        def __init__(self, w, x, y, z):
+            self.npvector = np.array([w, x, y, z])
+            self.w = self.npvector[0]
+            self.x = self.npvector[1]
+            self.y = self.npvector[2]
+            self.z = self.npvector[3]
+            [self.angle, self.axis] = self.get_axisangle()
+            
+        @classmethod
+        def from_npvector(cls, npvector):
+            return cls(npvector[0], npvector[1], npvector[2], npvector[3])
+
+        @classmethod
+        def from_axisangle(cls, axis, angle):
+            if not isinstance(axis, KOrderMath.Vector):
+                raise ValueError("Axis should be described as a KOrderMath.Vector")
+
+            theta = angle
+            axis = axis.normalize()
+
+            return cls._axisangle_to_q(axis, theta)
+
+        @classmethod
+        def as_pure(cls, axis):
+            if not isinstance(axis, KOrderMath.Vector):
+                raise ValueError("Axis should be described as a KOrderMath.Vector")
+
+            return KOrderMath.Quaternion(0.0, axis.x, axis.y, axis.z)
+
+        @classmethod
+        def _axisangle_to_q(cls, axis, angle):
+            w = cos(angle/2.)
+            x = axis.x * sin(angle/2.)
+            y = axis.y * sin(angle/2.)
+            z = axis.z * sin(angle/2.)
+
+            return KOrderMath.Quaternion(w,x,y,z)
+
+        # To string overload
+        def __str__(self):
+            return "angle: " + "{:.3f}".format(self.angle) + ", axis: " + str(self.axis)
         
+        def print_quat(self):
+            return "w: " + "{:.3f}".format(self.w) + "x: " + "{:.3f}".format(self.x) + ", y: " + "{:.3f}".format(self.y) + ", z: " + "{:.3f}".format(self.z)
+
+        def __mul__(self, b):
+            if isinstance(b, KOrderMath.Quaternion):
+                return self._multiply_with_quaternion(b)
+            elif isinstance(b, KOrderMath.Vector):
+                return self._multiply_with_vector(b)
+            else:
+                raise ValueError("Multiplication with unknown type " + str(type(b)))
+
+        def __rmul__(self, other):
+            return self.__mul__(other)
+
+        def get_conjugate(self):
+            conjugate = KOrderMath.Quaternion(self.w, -self.x, -self.y, -self.z)
+            return conjugate
+
+        def _multiply_with_quaternion(self, q2):
+            w1 = self.w
+            x1 = self.x
+            y1 = self.y
+            z1 = self.z
+
+            w2 = q2.w
+            x2 = q2.x
+            y2 = q2.y
+            z2 = q2.z
+            
+            w = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
+            x = w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2
+            y = w1 * y2 + y1 * w2 + z1 * x2 - x1 * z2
+            z = w1 * z2 + z1 * w2 + x1 * y2 - y1 * x2
+
+            result = KOrderMath.Quaternion(w, x, y, z)
+            return result
+
+        def _multiply_with_vector(self, v):
+            q = self.normalize()
+            q2 = KOrderMath.Quaternion.as_pure(v)
+            rotated_quaternion = (q.get_conjugate() * q2 * q)
+            return KOrderMath.Vector(rotated_quaternion.x, rotated_quaternion.y, rotated_quaternion.z)
+
     
 """
 SPACE FOR TESTING ONLY
@@ -134,4 +268,19 @@ print 2 * v
 print v * v2
 print v / 2.0
 print v // v2
+
+up = KOrderMath.Vector(0, 0, 1)
+
+q = KOrderMath.Quaternion.from_axisangle(up, KOrderMath.convert_degrees_to_radians(45.0))
+
+print q
+
+qconj = q.get_conjugate()
+print qconj
+
+right = KOrderMath.Vector(0, 1, 0)
+print "Before: " + str(right)
+right = right * q
+print "Rotated: " + str(right)
+
 """
