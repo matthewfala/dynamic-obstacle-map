839ee37e13ce790fee2cd61ac6dc446202194a18
Kishore Venkateshan <kishore.venkateshan1729@gmail.com>
2018-04-03 20:14:29 -0700

    Cherry picked and re-did mike's changes

--- Navigation/navsystem.py
+++ Navigation/navsystem.py
@@ -1,12 +1,14 @@
 import time
 
 import pathplanner as pp
+import simwriter as simwriter
 
 class NavSystem:
   def __init__(self):
     #Initialize World
 
     self.planner = pp.PathPlanner()
+    self.writer = simwriter.SimWriter("ticks.json")
 
     self.ticks = 0
     self.__UPDATES_PER_SECOND = 5.0
@@ -25,13 +27,20 @@ class NavSystem:
         self.ticks += 1
         currentTime = time.time() 
         if self.ticks > self.__SYNC_TICK_TIME:
-          self.update(currentTime - previousTime)
+          isOnline = self.update(currentTime - previousTime)
         previousTime = currentTime 
         nextTickTime += self.__TICK_DURATION
+
+   self.writer.writeFile()
  
   def update(self, deltaT):
       print "Updating Tick #: " +  str(self.ticks) + " with DeltaT= " + str(deltaT)
       self.planner.update(deltaT)
+      self.writer.append(self.ticks, self.planner.currentState)
+      #For testing only
+      if self.ticks > 30:
+         return False
+      return True
 
 navSystem = NavSystem()
 navSystem.run()
